5f6d34195c1b38c0ad5638d98852fa2dc11f46f7
C-Aoudair <aoudair16031999@gmail.com>
2024-07-05 11:31:26 +0100

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,10 +30,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """Constucture"""
         super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ Override the format method fo the parent class"""
         log_message = super().format(record)
         obfuscated_message = filter_datum(
             self.fields, self.REDACTION, log_message, self.SEPARATOR
