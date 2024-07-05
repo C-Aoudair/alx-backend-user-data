5157c86f0172df6cbd6bf2547e3ea45c16101617
C-Aoudair <aoudair16031999@gmail.com>
2024-07-05 02:21:54 +0100

    update filtered_logger.py: add RedactingFormatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 """ Contains filter_datum function"""
 
+import logging
 import re
 
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                message: str, seperator: str):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        seperator: str) -> str:
     """Returns the log message obfuscated."""
     for field in fields:
-        message = re.sub(rf"{field}=[^{seperator}]*",
-                        f"{field}={redaction}", message)
+        message = re.sub(
+                rf"{field}=[^{seperator}]*",
+                f"{field}={redaction}",
+                message
+            )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message = super().format(record)
+        obfuscated_message = filter_datum(
+            self.fields, self.REDACTION, log_message, self.SEPARATOR
+        )
+        return obfuscated_message
