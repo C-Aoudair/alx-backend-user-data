9fe3ed10d332e0fd23ae9568977a519e5e32e46f
C-Aoudair <aoudair16031999@gmail.com>
2024-07-06 01:50:56 +0100

    update encrypt_password.py: add is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Contains hash_password fucntion"""
+""" Contains hash_password and is_valid fucntions"""
 
 import bcrypt
 
@@ -12,3 +12,8 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
 
     return bcrypt.hashpw(bytes, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate a password and return True if it is valid"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
