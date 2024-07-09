5641541e3033f294df29cf710d6d41c7131321bc
C-Aoudair <aoudair16031999@gmail.com>
2024-07-09 01:21:13 +0100

    test

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,23 @@ from typing import List, TypeVar
 class Auth:
     """ Auth class for authentication porpuses"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False"""
+        """
+        Determines if authentication is required.
+        For now, this method returns False.
+        """
         return False
 
-    def authorization_header(self, request=None) -> None:
-        """ Returns None"""
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the value of the Authorization header from the request.
+        For now, this method returns None.
+        """
         return None
 
-    def current_user(self, request=None) -> None:
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user.
+        For now, this method returns None.
+        """
         return None
+
