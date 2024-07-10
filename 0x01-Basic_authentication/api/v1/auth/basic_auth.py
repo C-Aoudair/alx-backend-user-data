4e8b28b905f280e9f8a2bcd72ebd01278cb56363
C-Aoudair <aoudair16031999@gmail.com>
2024-07-10 02:56:43 +0100

    update clear
    claer

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ Contains BasicAuth class"""
 
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 import base64
 
 
@@ -68,8 +70,10 @@ class BasicAuth(Auth):
             (str, str): User email and password if valid,
             otherwise (None, None).
         """
-        if (decoded_base64_authorization_header is None or
-            not isinstance(decoded_base64_authorization_header, str)):
+        if (
+            decoded_base64_authorization_header is None or
+            not isinstance(decoded_base64_authorization_header, str)
+        ):
             return (None, None)
 
         if ':' not in decoded_base64_authorization_header:
@@ -77,3 +81,33 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        """
+        Retrieves a User instance based on email and password.
+
+        Args:
+            user_email (str): The user email.
+            user_pwd (str): The user password.
+
+        Returns:
+            TypeVar('User'): The User instance if valid, otherwise None.
+        """
+        if (
+            user_email is None or not isinstance(user_email, str) or
+            user_pwd is None or not isinstance(user_pwd, str)
+        ):
+            return None
+
+        user = User.search({'email': user_email})
+        if not user:
+            return None
+
+        user = user[0]
+
+        if user is None or not user.is_valid_password(user_pwd):
+            return None
+
+        return user
