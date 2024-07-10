0bc6edc873bf120aff5af821c3796d231dd7b436
C-Aoudair <aoudair16031999@gmail.com>
2024-07-10 03:10:08 +0100

    upate api/v1/auth/basic_auth.py: add current_user  method for BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -108,5 +108,33 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
- 
+
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overloads Auth and retrieves the User instance for a request.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            TypeVar('User'): The User instance if valid, otherwise None.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+
+        return self.user_object_from_credentials(email, password)
