b41941dff11210d592329b1fa51e167a6f31f867
C-Aoudair <aoudair16031999@gmail.com>
2024-07-17 18:26:17 +0100

    add auth.py

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Auth module"""
+
+import bcrypt
+
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """hashs the password and return the bytes form"""
+    bytes = password.encode('utf-8')
+
+    return bcrypt.hashpw(bytes, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register returns a user with email and password"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
