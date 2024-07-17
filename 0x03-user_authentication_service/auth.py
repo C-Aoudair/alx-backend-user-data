3612f07edd194797e07d4552a74b4097fa4a6dea
C-Aoudair <aoudair16031999@gmail.com>
2024-07-17 18:45:18 +0100

    update auth.py: add valid_login method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,8 +15,6 @@ def _hash_password(password: str) -> bytes:
     Returns:
         bytes: the hashed password
     """
-    if not password or not isinstance(password, str):
-        return None
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
@@ -35,3 +33,11 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid_login returns a boolean if the password is correct"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
