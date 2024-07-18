03e0518d6af329cfd22d7ca6c451a1177f43274a
C-Aoudair <aoudair16031999@gmail.com>
2024-07-18 02:31:30 +0100

    test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,13 +43,13 @@ def login():
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
-    """Logout route"""
+    """Logout route for logout the user"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
         return redirect("/")
-    return make_response("", 403)
+    return abort(403)
 
 
 if __name__ == "__main__":
